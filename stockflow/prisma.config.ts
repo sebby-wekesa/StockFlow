4a6baef884d43282ddd2ddc69af58e2e3c3875b6
sebby wekesa <sebbywakis@gmail.com>
2026-04-09 15:13:20 +0300

    implementing the database

--- stockflow/prisma.config.ts
+++ stockflow/prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "@prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
+    provider: "postgresql",
     url: process.env.DATABASE_URL!,
   },
 });
