4782f670632d6017f2f1a116f2d1da269759148b
sebby wekesa <sebbywakis@gmail.com>
2026-04-15 16:58:45 +0300

    final stage

--- stockflow/next.config.ts
+++ stockflow/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverExternalPackages: ['@prisma/client'],
+  },
 };
 
 export default nextConfig;
