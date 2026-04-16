239ad0f963a400e6c51f8064888f910c0f505d29
sebby wekesa <sebbywakis@gmail.com>
2026-04-16 13:06:15 +0300

    1new fix: separated prisma logic into server actions

--- stockflow/lib/prisma.ts
+++ stockflow/lib/prisma.ts
@@ -1,7 +1,22 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prismaClientSingleton = () => {
+  const options: any = {
+    log: ['error'],
+  }
+  if (process.env.DATABASE_URL) {
+    options.adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+  }
+  return new PrismaClient(options)
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
