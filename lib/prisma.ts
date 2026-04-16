f7f3b36717db1ca00fcaf2b0176029c6ddfb459b
sebby wekesa <sebbywakis@gmail.com>
2026-04-16 15:20:43 +0300

    fix1: total reset of middleware to remove __dirname dependencies

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,22 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prismaClientSingleton = () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is required')
+  }
+  return new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    log: ['error'],
+  })
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
